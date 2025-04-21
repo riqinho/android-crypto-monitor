2680e5d31e5495ef343e1c3ceecf4454800088b0
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-04-21 19:32:05 -0300

    Limpando códigos não necessários da MainActivity.kt

--- app/src/main/java/riqinho/com/github/cryptomonitor/MainActivity.kt
+++ app/src/main/java/riqinho/com/github/cryptomonitor/MainActivity.kt
@@ -16,32 +16,6 @@ import riqinho.com.github.cryptomonitor.ui.theme.CryptoMonitorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CryptoMonitorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoMonitorTheme {
-        Greeting("Android")
     }
 }
