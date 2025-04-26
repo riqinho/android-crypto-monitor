e13213bc641d48553c8189cec4fe0853edd04e4f
Rick Alves Domingues <rickpoterdom@gmail.com>
2025-04-26 20:47:56 -0300

    Configurando a MainActivity.kt para executar com o layout pronto

--- app/src/main/java/riqinho/com/github/cryptomonitor/MainActivity.kt
+++ app/src/main/java/riqinho/com/github/cryptomonitor/MainActivity.kt
@@ -1,21 +1,24 @@
 package riqinho.com.github.cryptomonitor
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import riqinho.com.github.cryptomonitor.ui.theme.CryptoMonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        //configurando toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+
+    private fun configureToolbar(toolbar: Toolbar){
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
